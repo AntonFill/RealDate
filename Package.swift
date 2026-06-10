794d34ee979dbaeb6fcf09b7859fd0367f2407ca
Anton Fillmann <anton.fillmann@icloud.com>
2026-06-10 10:17:19 +0200

    fixed critical issue about testing in terminal (by cleanup testing-module dependency).

--- Package.swift
+++ Package.swift
@@ -6,11 +6,10 @@ import PackageDescription
 let package = Package(
     name: "realdate",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.1"),
-        .package(url: "https://github.com/apple/swift-testing", from: "0.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.8.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -24,8 +23,7 @@ let package = Package(
         .testTarget(
             name: "realdateTests",
             dependencies: [
-                "realdate",
-                .product(name: "Testing", package: "swift-testing"),
+                "realdate"
             ]
         ),
     ],
